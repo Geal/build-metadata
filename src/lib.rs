c4b1e3ea9f8c5ce92af158943b8300f353ca0bdf
Geoffroy Couprie <geo.couprie@gmail.com>
2017-08-28 12:29:34 +0200

    add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! use this crate to embed repository and build metadata at compile time
+//!
+//! This project uses procedural macros to provide you with this metadata,
+//! exposing three macros:
+//!
+//! * `head!()`: the name of the current git branch or tag you are in
+//! * `commit!()`: short git commit id
+//! * `time!()`: UTC build time. This value is cached, so it will stay the same for every call in one crate. Note that using this makes the build non reproducible (since the resulting binary will change depending on build time)
+//!
+//! # Example
+//!
+//! ```rust
+//! #![feature(plugin)]
+//! #![plugin(build_metadata)]
+//!
+//! fn main() {
+//!     println!("build time: {}", time!());
+//!     println!("head: {}", head!());
+//!     println!("commit id: {}", commit!());
+//! }
+//! ```
+//!
 #![feature(plugin_registrar, rustc_private)]
 
 #[macro_use]
