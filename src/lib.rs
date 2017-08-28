396cf3b88cc770e2512b1322bfb6e76082186f72
Geoffroy Couprie <geo.couprie@gmail.com>
2017-08-28 11:36:21 +0200

    fix compilation

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,9 @@ extern crate syntax;
 use rustc_plugin::Registry;
 use syntax::tokenstream::TokenTree;
 use syntax::codemap::Span;
-use syntax::ext::base::{ExtCtxt, DummyResult, MacResult};
+use syntax::symbol::Symbol;
+use syntax::ext::base::{self, ExtCtxt, DummyResult, MacResult};
+use syntax::ext::build::AstBuilder;
 use syntax::print::pprust::tt_to_string;
 
 #[plugin_registrar]
@@ -15,7 +17,7 @@ pub fn plugin_registrar(reg: &mut Registry) {
     reg.register_macro("commit", commit);
 }
 
-fn commit<'a>(cx: &'a mut ExtCtxt, sp: Span, args: &[TokenTree]) -> Box<MacResult + 'a> {
+fn commit<'a>(cx: &'a mut ExtCtxt, sp: Span, tts: &[TokenTree]) -> Box<MacResult + 'a> {
     base::check_zero_tts(cx, sp, tts, "commit!");
 
     let topmost = cx.expansion_cause().unwrap_or(sp);
